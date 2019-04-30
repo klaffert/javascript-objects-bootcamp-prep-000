8c5437b9ca70048327d4ebaae5f00a255fe5a2c9
Kelliann Lafferty <kell10192@gmail.com>
2019-04-30 23:25:15 +0000

    Automatically backed up by Learn

---
+++ objects.js
@@ -0,0 +1,15 @@
+var meals = {};
+var meals = new Object();
+
+var meals = {breakfast: "oatmeal"};
+// or, equivalently
+var meals = new Object ({breakfast: 'oatmeal'})
+
+var meals = {
+  breakfast: 'eggs',
+  breakfast: 'bacon'
+}
+
+var playlist = {
+  Cher: "Believe in Love"
+}
